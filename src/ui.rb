40cc3e19827639773be226d41429f10795cd6676
danm <writetodan@yahoo.com>
2014-11-17 13:25:52 +0000

    @damned android working nicer: refined game loop; visible visited locations; basic button rendering; selecting only touch-down events

--- src/ui.rb
+++ src/ui.rb
@@ -3,9 +3,10 @@ require 'monitor'
 module RubotoUi
 
   class Scene < android.view.View
-    java_import "android.graphics.Color"
-    java_import "android.graphics.Paint"
-    java_import "android.graphics.RectF"
+    java_import 'android.graphics.Color'
+    java_import 'android.graphics.Paint'
+    java_import 'android.graphics.RectF'
+    java_import 'android.view.MotionEvent'
 
     include MonitorMixin
 
@@ -23,32 +24,35 @@ module RubotoUi
 
     def draw_location(location)
       @drawers << ->(canvas) {
-        canvas.drawCircle(location.x, location.y, 20, @location_paint)
+        visited = location.state == :visited
+        canvas.drawCircle(location.x, location.y, 20, visited ? blue_paint : red_paint)
       }
     end
 
     def draw_actor(actor)
       @drawers << ->(canvas) {
-        canvas.drawCircle(actor.x, actor.y, 20, @actor_paint)
+        canvas.drawCircle(actor.x, actor.y, 20, white_paint)
       }
     end
 
     def draw_circle(point, radius)
       @drawers << ->(canvas) {
-        canvas.drawCircle(point.x, point.y, radius, @paint)
+        canvas.drawCircle(point.x, point.y, radius, yellow_paint)
       }
     end 
 
     def draw_patch(patch)
-      puts patch.to_s
+#      puts patch.to_s
     end
 
     def draw_button(button)
-      puts button.to_s
+      @drawers << ->(canvas) {
+        canvas.drawCircle(button.point.x, button.point.y, 30, yellow_paint)
+      }
     end
     
     def onDraw(canvas)
-      puts "in onDraw !!!!!!!!!!!!!!!!!!!!!!! xxxxxxxxxxxxxxxxxxxx"
+      puts "onDraw: size: #{get_width}, #{get_height}"
       canvas.drawColor(Color::BLACK)
       synchronize do
         @drawers.each do |drawer|
@@ -58,10 +62,11 @@ module RubotoUi
     end
 
     def onTouchEvent(event)
-      puts "got a touch event!"
       puts event
-      
-      game.event :press, point: Point.new(event.get_x, event.get_y)
+
+      if event.get_action_masked == MotionEvent::ACTION_DOWN      
+        game.event :press, point: Point.new(event.get_x, event.get_y)
+      end
 
       puts 'handled event ok'
       true
@@ -69,15 +74,21 @@ module RubotoUi
 
     private
 
+    attr_reader :base_paint, :blue_paint, :red_paint, :yellow_paint, :white_paint
+
     def init_paints
-      base_paint = Paint.new
+      @base_paint = Paint.new
       base_paint.anti_alias = true
-      @paint = Paint.new(base_paint)
-      @paint.color = Color::YELLOW
-      @actor_paint = Paint.new(base_paint)
-      @actor_paint.color = Color::WHITE
-      @location_paint = Paint.new(base_paint)
-      @location_paint.color = Color::BLUE
+      @yellow_paint = paint(Color::YELLOW)
+      @white_paint = paint(Color::WHITE)
+      @blue_paint = paint(Color::BLUE)
+      @red_paint = paint(Color::RED)
+    end
+
+    def paint(color)
+      paint = Paint.new(base_paint)
+      paint.color = color
+      paint
     end
   end
 
@@ -89,14 +100,22 @@ module RubotoUi
       scene.game = game
     end
     def start(&block)
-      interval = 0.1
+      interval = 0.03
+      last_tick = 0.0
       while true do
-        puts "time: #{Time.now.to_f}"
         scene.synchronize do
           game.instance_exec interval, &block
         end
+        tick_time = Time.now.to_f - last_tick
+        
+        early_by = interval - tick_time
+        sleep early_by if early_by > 0
+
+        puts "tick: early by #{early_by}, time taken #{tick_time}"
+
+        last_tick = Time.now.to_f
+
         scene.postInvalidate
-        sleep interval
       end
     end
   end
